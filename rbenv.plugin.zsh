e875ff2c6d7a84d19139b87eec150b6ac852a5a2
Salvydas Lukosius <sall@w-ss.io>
2022-05-10 08:26:13 +0100

    Update rbenv.plugin.zsh

--- rbenv.plugin.zsh
+++ rbenv.plugin.zsh
@@ -1,8 +1,13 @@
-# According to the Zsh Plugin Standard:
-# https://digitalclouds.dev/docs/zsh/Zsh-Plugin-Standard.html
-
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-
-# Then ${0:h} to get plugin's directory
+#  ============================================================================  #
+#  [ https://github.com/z-shell ] ❮ ZI ❯         [ (c) 2022 Z-SHELL COMMUNITY ]  #
+#  ============================================================================  #
+#
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+#
+# Standardized $0 Handling
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
+0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
 eval "$(rbenv init -)"
